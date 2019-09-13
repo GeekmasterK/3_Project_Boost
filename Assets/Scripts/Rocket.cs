0c5015211d217db149c263272a782991717a7ae3
GeekmasterK <ktheiss1990@gmail.com>
2019-09-13 09:32:48 -0700

    Thruster audio added. Movement bugs fixed.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,31 +6,52 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
 
+    AudioSource audioSource;
+
+    bool thrusterActive;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
