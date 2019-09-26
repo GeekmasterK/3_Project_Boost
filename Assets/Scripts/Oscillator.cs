57d11fe49861e28978ed040eccf7a41f3e833fdc
Tarsonis\Kevin <ktheiss1990@gmail.com>
2019-09-26 16:14:10 -0700

    Automatic oscillation added to script.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
-    // TODO remove from inspector later
-    [Range(0, 1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved.
+    float movementFactor; // 0 for not moved, 1 for fully moved.
 
     Vector3 startingPos; // must be stored for absolute movement
 
@@ -21,6 +21,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // TODO protect against period is 0
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2f; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
